9390d6cec5fd370bde38eda475fc488232619ac7
JiwooKimAR <kjiwoo1210@gmail.com>
2023-11-01 16:56:22 +0000

    delete old files

--- run_t5/scripts/t5_exp3.sh
+++
@@ -1,8 +0,0 @@
-python inference.py --label exp3_pretrained_unifiedqa \
---test_split test \
---test_num -1 \
---model large \
---gpu 0 
-
-# 15.96%
-python eval.py --result_file t5/exp3_pretrained_unifiedqa_large.json
